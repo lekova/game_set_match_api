cef6cbae246a19d8a22386c09f9321dbcd462781
lekova <lina.lekova@gmail.com>
2015-09-07 13:21:26 -0400

    Change table name from user_proficiency to user_proficiency_types

--- db/migrate/20150906160759_associate_user_with_proficiency.rb
+++ db/migrate/20150906160759_associate_user_with_proficiency.rb
@@ -1,6 +1,6 @@
 class AssociateUserWithProficiency < ActiveRecord::Migration
   def change
-    create_table :user_proficiency do |t|
+    create_table :user_proficiency_types do |t|
       t.references :user, index: true, foreign_key: true
       t.references :proficiency_type, index: true, foreign_key: true
       t.string :user_comment
