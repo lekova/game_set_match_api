728908cc62c1e66ab936cc8c9247d1bd0f61725a
lekova <lina.lekova@gmail.com>
2015-09-07 13:33:34 -0400

    Add delete dependency on associations for user-address and user-proficiency

--- db/migrate/20150906155250_associate_user_with_address.rb
+++ db/migrate/20150906155250_associate_user_with_address.rb
@@ -1,8 +1,8 @@
 class AssociateUserWithAddress < ActiveRecord::Migration
   def change
     create_table :user_addresses do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :address, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true, :dependent => :delete
+      t.references :address, index: true, foreign_key: true, :dependent => :delete
       t.string :name
 
       t.timestamps null: false
--- db/migrate/20150906160759_associate_user_with_proficiency.rb
+++ db/migrate/20150906160759_associate_user_with_proficiency.rb
@@ -1,8 +1,8 @@
 class AssociateUserWithProficiency < ActiveRecord::Migration
   def change
     create_table :user_proficiency_types do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :proficiency_type, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true, :dependent => :delete
+      t.references :proficiency_type, index: true, foreign_key: true, :dependent => :delete
       t.string :user_comment
 
       t.timestamps null: false
