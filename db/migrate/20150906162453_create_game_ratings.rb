7889263c25d1f8baa515d7ccab3d1bff67f2bc34
lekova <lina.lekova@gmail.com>
2015-09-07 14:17:49 -0400

    Add unique index constraint to game_rating for game _id and user_id
    This means that one user can rate a game only once.
    Add user_comment column.

--- db/migrate/20150906162453_create_game_ratings.rb
+++ db/migrate/20150906162453_create_game_ratings.rb
@@ -5,8 +5,10 @@ class CreateGameRatings < ActiveRecord::Migration
       t.references :user, :column => :rating_player_id, foreign_key: true, :dependent => :delete
       t.references :user, :column => :rated_player_id, foreign_key: true, :dependent => :delete
       t.integer :rate
+      t.text :user_comment
 
       t.timestamps null: false
     end
+    add_index :game_ratings, [:user_id, :game_id], :unique => true
   end
 end
