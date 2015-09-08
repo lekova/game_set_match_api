9f8705c1d1cb686431567da9df6856950e79de46
lekova <lina.lekova@gmail.com>
2015-09-08 12:18:49 -0400

    Add user_serializer to add proficiency_level to user object.

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,13 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :name, :age, :gender, :about_me, :image, :current_proficiency_type, :all_time_proficiency_types
+
+  def current_proficiency_type
+    # returns last proficiency_type for current user
+    object.proficiency_types.last
+  end
+
+  # def all_time_proficiency_types
+  #   # returns all levels over time for current user
+  #   object.proficiency_types
+  # end
+end
