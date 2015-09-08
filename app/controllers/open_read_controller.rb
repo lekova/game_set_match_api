affadcaceebd2fe5a428d1c173b1671d5dd2d535
lekova <lina.lekova@gmail.com>
2015-09-08 12:14:28 -0400

    Add open read controller for user authentication.

---
+++ app/controllers/open_read_controller.rb
@@ -0,0 +1,13 @@
+class OpenReadController < ApplicationController
+  # include ActionController::HttpAuthentication::Token
+
+  skip_before_action :authenticate, only: [:index, :show]
+  before_action :set_current_user, only: [:index, :show]
+
+  private
+  def set_current_user
+    return if @current_user
+    header = request.headers['Authorization']
+    @current_user = User.find_by(token: header.split('=')[1]) if header
+  end
+end
