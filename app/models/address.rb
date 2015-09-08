0c8ece7d66021dd7be8d76844f33c0b86d1c6fb1
lekova <lina.lekova@gmail.com>
2015-09-08 12:09:11 -0400

    Add dependent :destroy to address and proficiency type models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   has_many :users, through: :user_addresses
-  has_many :user_addresses
+  has_many :user_addresses, dependent: :destroy
 
   validates :zipcode, presence: true
 end
