927298dff1dc0b20fff1f0b318778a4b2c7e1282
lekova <lina.lekova@gmail.com>
2016-01-21 11:18:41 -0500

    Add has_many proficiency_types in user serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :age, :gender, :about_me, :image, :current_proficiency_type
   # , :all_time_proficiency_types
   has_many :addresses
+  has_many :proficiency_types
 
   def current_proficiency_type
     # returns last proficiency_type for current user
