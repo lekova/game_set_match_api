7f18f17ef162b16d9773a4c3be6a9f4d96c40c0b
lekova <lina.lekova@gmail.com>
2016-01-29 17:31:12 -0500

    Change the create and update methods in addresses controller.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,7 @@
 class AddressesController < OpenReadController
   def index
     puts "#{current_user.addresses}"
-    addresses = User.find(params[:id]).addresses
+    addresses = current_user.addresses
     render json: addresses
   end
 
@@ -11,23 +11,17 @@ class AddressesController < OpenReadController
   end
 
   def create
-    address = Address.create(addr_params)
+    address = current_user.addresses.create(addr_params)
     if address.save!
-      user_address = current_user.user_addresses.create(address_id: address.id, name: addr_name_params[:name])
-      if user_address.save!
-        render json: address
-      else
-        render json: user_address.errors, status: :unprocessable_entity
-      end
+      render json: address
     else
       render json: address.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    # TODO
-    address = Address.find(params[:id])
-    if address.update!(address_params)
+    address = Address.find(addr_params[:id])
+    if address.update!(addr_params)
       render json: address
     else
       render json: address.errors, status: :unprocessable_entity
@@ -36,17 +30,13 @@ class AddressesController < OpenReadController
 
   def destroy
     # TODO
-    address = Address.find(params[:id])
+    address = Address.find(addr_params[:id])
     address.destroy!
   end
 
   private
 
   def addr_params
-    params.permit(:id, :street, :city, :state, :country, :zipcode)
-  end
-
-  def addr_name_params
-    params.permit(:name)
+    params.permit(:id, :street, :city, :state, :country, :zipcode, :name)
   end
 end
